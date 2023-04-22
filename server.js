2e1161e8900dd3bc3f60f42dd4a298dcd11ea043
Ishan D. Gupta <ishan.d.gupta@outlook.com>
2023-04-22 20:38:58 +0000

    - serve homepage via route

--- server.js
+++ server.js
@@ -5,16 +5,15 @@ const express = require('express')
 const app = express()
 
 // database config
-const connection = require('./config/database')
-connection.once('open', ()=>{console.log('Database Connected')})
-connection.on('error', ()=>{console.log('Database Connection Error')})
+// const connection = require('./config/database')
+// connection.once('open', ()=>{console.log('Database Connected')})
+// connection.on('error', ()=>{console.log('Database Connection Error')})
 
 // routes
-// parse in json format
-app.use(express.json({extended: false}))
-app.use('/', require('./routes/redirect'))
-app.use('/api/url', require('./routes/url'))
+app.use('/', (req, res) => {
+    res.sendFile('index.html', {root: 'public'})
+})
 
 // listen for requests
 const PORT = process.env.PORT || 5000
-app.listen(PORT, ()=>{console.log('server started at port ' + PORT)})
+app.listen(PORT, () => { console.log('server started at port ' + PORT) })
